660423e470fb6d90735d5d9100160a5fbe303d39
aperalta <aperalta@aperalta-0365df>
2013-07-23 20:58:47 +0200

    Libreria de Utilidades, Empieza con encriptacion en MD5

---
+++ timmps_util/src/es/timmps/util/Encriptacion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.timmps.util;
+
+import es.timmps.exceptions.TimmpsException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author aperalta
+ */
+public class Encriptacion {
+    
+    
+    public static String findMD5(String arg) throws TimmpsException {
+
+        MessageDigest algorithm = null;
+        try {
+            algorithm = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException nsae) {
+            throw new TimmpsException("No se a encontrado el alogaritmo ",nsae);
+        }
+        byte[] defaultBytes = arg.getBytes();
+        algorithm.reset();
+        algorithm.update(defaultBytes);
+        byte messageDigest[] = algorithm.digest();
+        StringBuffer hexString = new StringBuffer();
+
+        for (int i = 0; i < messageDigest.length; i++) {
+            String hex = Integer.toHexString(0xff & messageDigest[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
